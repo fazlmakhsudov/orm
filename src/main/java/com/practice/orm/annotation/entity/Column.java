a389637c5e556c088935e9af6b66dbc8066f6d13
mvp <dima.bobryshev.mvp@gmail.com>
2020-04-24 14:52:33 +0300

    base methods to entity

--- src/main/java/com/practice/orm/annotation/entity/Column.java
+++ src/main/java/com/practice/orm/annotation/entity/Column.java
@@ -7,8 +7,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Column{
-
-    String name();
-
+public @interface Column {
+    String name() default "";
+    boolean nullable() default true;
 }
--- src/main/java/com/practice/orm/annotation/entity/Handler.java
+++ src/main/java/com/practice/orm/annotation/entity/Handler.java
@@ -1,4 +1,56 @@
 package com.practice.orm.annotation.entity;
 
+import org.reflections.Reflections;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Handler {
+    private static Reflections reflections = new Reflections("NamePackage");
+
+    public static Set<Class<?>> getClassesNamedEntity() {
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Entity.class);
+        return classes;
+    }
+
+    public static Set<Field> getFieldsNamedByAnnotation(Class<?> classes,
+                                                        Class<? extends Annotation> annotation)
+    {
+        Set<Field> fields = Arrays.stream(classes.getDeclaredFields())
+                .filter(field ->field.isAnnotationPresent(annotation))
+                .peek(field -> field.setAccessible(true))
+                .collect(Collectors.toSet());
+        return fields;
+    }
+
+    public static Set<Field> getFieldsByClass(Class<?> clazz)
+    {
+        Set<Field> fields = Arrays.stream(clazz.getDeclaredFields())
+                .collect(Collectors.toSet());
+        return fields;
+    }
+
+    public static String getTableName(Class<?> clazz)
+    {
+        String name = null;
+        if (clazz.isAnnotationPresent(Table.class))
+        {
+            name = clazz.getAnnotation(Table.class).name();
+        }
+        else
+        {
+            name = clazz.getSimpleName();
+        }
+        return name;
+    }
+
+    public static Field getFieldNamedId(Class<?> clazz){
+        Field idField = Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Id.class))
+                .findFirst().get();
+        return idField;
+    }
 }
